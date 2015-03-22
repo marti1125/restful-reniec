8ddc8399d736b8b2b823f9c9cd93a36902fcfd85
Willy Aguirre <marti1125@gmail.com>
2015-03-22 17:54:40 -0500

    se agrega los archivos necesarios al proyecto

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # restful-reniec
 un servicio prototipo para validar el dni
+
+http://localhost/restful-reniec/index.php/verificar/48754156
---
+++ index.php
@@ -0,0 +1,41 @@
+<?php
+
+header('Access-Control-Allow-Origin: *');
+header('content-type: application/json; charset=utf-8');
+
+//Conexión a base de datos Mysql
+
+$host = 'localhost';
+$username= 'root';
+$password = 'mysql';
+$db_name = 'reniec';
+
+$db=mysql_connect($host, $username, $password) or die('No se puedo conectar a la base de datos...');
+
+mysql_select_db($db_name, $db) or die('');
+
+require 'Slim/Slim.php';
+
+\Slim\Slim::registerAutoloader();
+
+$app = new \Slim\Slim();
+
+$app->get('/verificar/:dni', function ($dni) use ($app) {
+
+    $sth = mysql_query("select * from persona where dni = '".$dni."'");   
+    
+	$rows = array();
+	while($r = mysql_fetch_assoc($sth)) {
+	    $rows[] = $r;
+	}
+
+	echo json_encode($rows);
+	exit;
+
+});
+
+$app->get('/', function () {
+    echo "RENIEC";
+});
+
+$app->run();
